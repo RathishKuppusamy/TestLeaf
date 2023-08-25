e6c4b7960887cc454009c945c328d75a6cca4f19
RathishKuppusamy <142977846+RathishKuppusamy@users.noreply.github.com>
2023-08-25 13:17:36 +0530

    Add files via upload
    Basic Selenium code for setup

---
+++ day1/SeleniumPilot.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumPilot {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.quit();
+		
+		System.out.println("Executed Successfiully");
+	}
+
+}
